4756084eab93ef3dc838f5fc51c273a1d73f0662
Derek Rice <derekrice@arizona.edu>
2023-07-16 08:20:19 -0700

    Part V - 2nd attempt - edit in R studio and push to base repo and fork

--- INFO_TEST_A.R
+++ INFO_TEST_A.R
@@ -1,17 +1,2 @@
-install.packages("rmarkdown")
-library(rmarkdown)
----
-  title: "Hello R Markdown"
-author: "Awesome Me"
-date: "2018-02-14"
-output: html_document
----
-  
-```{r}
-fit = lm(dist ~ speed, data = cars)
-b   = coef(fit)
-plot(cars)
-abline(fit)
-```
+## info test initial file - edt Sunday Morning 
 
-The slope of the regression is `r b[1]`.
